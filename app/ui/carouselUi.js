ddf1bcaad98d57141442f18c7eba7f2db5bb3025
Reiggnar <bruno.remy.sebastien@gmail.com>
2024-03-20 14:50:16 +0100

    first commit

---
+++ app/ui/carouselUi.js
@@ -0,0 +1,9 @@
+import styles from "./carouselui.module.css"
+
+export default function CarouselUi() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Carousel</h2>
+    </div>
+  )
+}
